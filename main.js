5dd52504aa8941fc8e7cdc4d744a1c49ad3739f0
pedrovolei <133118313+pedrovolei@users.noreply.github.com>
2023-11-07 08:51:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 } 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
 let contador = 0;
 //Estrutura de repetição - Enquanto
 while(contador <listaDeTeclas.length){
-    const instrumento = listaDeTeclas[contador].classList[1];
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom('#som_tecla_splash');
-    };
-    contador = contador + 1;
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
     const idAudio = `#som_${istrumento}`;
+        console.log(idAudio);
+
+        tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+
+    contador = contador + 1;
     console.log(instrumento)
 }
